00cfbb2cc392308b9d5c095d7fcbf856640e2495
arunachalam-n <oldwine357@gmail.com>
2020-12-11 17:43:08 +0530

    allowed host

--- demo/settings.py
+++ demo/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '=78w#xhdwd!hwnb11d^@z$u30x9f^%a(ktrex=u$dx9_c!igsv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 # Application definition
 
